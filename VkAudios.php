e79b792ba583e21af9beba6e2787d467f18af8ac
Victor <scherbackov.victor@yandex.ru>
2016-01-25 08:21:11 +0300

    Some Updates
    - Some functions Added in VkAudios.php
    - Fixed tabulation in VkBot.php
    - Some corrects in VkBot.php

--- VkAudios.php
+++ VkAudios.php
@@ -2,32 +2,100 @@
 
 class VkAudios extends VkBot{
 
-    public function __construct($token){
-        parent::__construct($token);
-    }
+	private $genres = [
+			1 => "Rock", 2 => "Pop", 3 => "Rap & Hip-Hop", 4 => "Easy Listening", 5 => "Dance & House", 6 => "Instrumental", 7 => "Metal", 21 => "Alternative", 8 => "Dubstep", 9 => "Jazz & Blues", 10 => "Drum & Bass", 11 => "Trance", 12 => "Chanson", 13 => "Ethnic", 14 => "Acoustic & Vocal", 15 => "Reggae", 16 => "Classical", 17 => "Indie Pop", 19 => "Speech", 22 => "Electropop & Disco", 18 => "Other"
+		];
+
+	public function __construct($token){
+		parent::__construct($token);
+	}
 
     /**
      * @param $id
      * @return bool
      */
     public function getNameGenreById($id){
-        $genres = [
-            1 => "Rock", 2 => "Pop", 3 => "Rap & Hip-Hop", 4 => "Easy Listening", 5 => "Dance & House", 6 => "Instrumental", 7 => "Metal", 21 => "Alternative", 8 => "Dubstep", 9 => "Jazz & Blues", 10 => "Drum & Bass", 11 => "Trance", 12 => "Chanson", 13 => "Ethnic", 14 => "Acoustic & Vocal", 15 => "Reggae", 16 => "Classical", 17 => "Indie Pop", 19 => "Speech", 22 => "Electropop & Disco", 18 => "Other"
-        ];
-        if(isset($genres[$id])){
-            return true;
-        }else{
-            return false;
-        }
-    }
+		if(isset($this->genres[$id])){
+			return $this->genres[$id];//true;
+		}else{
+			return false;
+		}
+	}
 
     /**
      * @return array
      */
     public function getGenres(){
-        return $genres = [
-            1 => "Rock", 2 => "Pop", 3 => "Rap & Hip-Hop", 4 => "Easy Listening", 5 => "Dance & House", 6 => "Instrumental", 7 => "Metal", 21 => "Alternative", 8 => "Dubstep", 9 => "Jazz & Blues", 10 => "Drum & Bass", 11 => "Trance", 12 => "Chanson", 13 => "Ethnic", 14 => "Acoustic & Vocal", 15 => "Reggae", 16 => "Classical", 17 => "Indie Pop", 19 => "Speech", 22 => "Electropop & Disco", 18 => "Other"
-        ];
+        return $this->genres;
     }
 
+	/**
+     * @param int $owner_id 	- ID of the user or community that owns the audio file. Use a negative value to designate a community ID. (current user id is used by default)
+	 * @param int $album_id 	- Audio album ID.
+	 * @param string $audio_ids - IDs of the audio files to return. (list of comma-separated positive numbers)
+	 * @param bool $need_user 	- true — to return information about users who uploaded audio files (default: true)
+	 * @param int $offset 		- Offset needed to return a specific subset of audio files. (default: 0)
+	 * @param int $count 		- Number of audio files to return. (default: 6000, max: 6000)
+	 * @throws VkException
+	 * @return array
+	 */
+	public function getAudios($owner_id = null, $album_id, $audio_ids = '', $need_user = true, $offset = 0, $count = 6000){
+		$r = array(
+			"album_id" => $album_id,
+			"audio_ids" => $audio_ids,
+			"need_user" => $need_user ? "1" : "0",
+			"offset" => $offset,
+			"count" => $count
+		)
+		if(!is_null($owner_id)){ $r["owner_id"] = $owner_id; }
+		return $this->api('audio.get',$r);
+	}
+	
+	/**
+     * @param string $request 	- Audio file IDs, in the following format: {owner_id}_{audio_id} (list comma-separated strings)
+	 * @throws VkException
+	 * @return array
+	 */
+	public function getAudiosById($request){
+		return $this->api("audio.getById",array("audios" => $request));
+	}
+	
+	/**
+     * @param int $lyrics_id 	- Lyrics ID. 
+	 * @throws VkException
+	 * @return array
+	 */
+	public function getLyrics($lyrics_id){
+		return $this->api("audio.getLyrics",array("lyrics_id" => $lyrics_id));
+	}
+	
+	/**
+     * @param string $q 			- Search query string (e.g., The Beatles).
+     * @param bool $auto_complete 	- true — to correct for mistakes in the search query (e.g., if you enter Beetles, the system will search for Beatles) 
+     * @param bool $lyrics 			- true — to return only audio files that have associated lyrics
+     * @param bool $performer_only 	- true — to search only by artist name
+     * @param int $sort 			- Sort order:
+	 * 									1 — by duration
+	 * 									2 — by popularity
+	 * 									0 — by date added
+	 * @param bool $search_own 		- 
+	 * @param int $offset 			- Offset needed to return a specific subset of audio files. (default: 0)
+	 * @param int $count			- Number of audio files to return. (default: 30, max: 300)
+	 * @throws VkException
+	 * @return array
+	 */
+	
+	public function search($q, $auto_complete, $lyrics, $performer_only, $sort, $search_own, $offset = 0, $count = 30){
+		$r = array(
+			"q" => $q,
+			"auto_complete" => $auto_complete,
+			"lyrics" => $lyrics,
+			"performer_only" => $performer_only,
+			"sort" => $sort,
+			"search_own" => $search_own,
+			"offset" => $offset,
+			"count" => $count
+		);
+		return $this->api("audio.search",$r);
+	}
 }
--- VkBot.php
+++ VkBot.php
@@ -3,129 +3,131 @@
 
 class VkBot{
 
-    const API_VERSION = '5.24';
-    const METHOD_URL = 'https://api.vk.com/method/';
+	const API_VERSION = '5.24';
+	const METHOD_URL = 'https://api.vk.com/method/';
 
-    private $access_token = "";
-    private $scopes = [];
+	private $access_token = "";
+	private $scopes = [];	
     //Classes
-    /** @var  VkAudios */
-    private $audios;
-    public function __construct($token = null){
-        if(!is_null($token)){
-            $this->access_token = $token;
-        }
-        $this->init_classes();
+	/** @var  VkAudios */
+	private $audios;
+	public function __construct($token = null){
+		if(!is_null($token)){
+			$this->access_token = $token;
+		}
+		$this->init_classes();
+	}
+
+	public function init_classes(){
+		$this->audios = new VkAudios($this->access_token);
+		//TODO: More Classes
+	}
+
+
+	/**
+	 * @return null|string
+	 */
+	public function getAccessToken(){
+		return $this->access_token;
+	}
+
+	/**
+	 * @param null|string $access_token
+	 */	
+	public function setAccessToken($access_token){
+		$this->access_token = $access_token;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getScopes(){
+		return $this->scopes;
+	}
+
+	/**
+	 * @param array $scopes
+	 */
+	public function setScopes($scopes){
+		$this->scopes = $scopes;
+	}
+
+	/**
+	 * @param string $method - �����, http://vk.com/dev/methods
+	 * @param array $vars
+	 * @return array
+	 */
+	public function api($method = '', array $vars = array()){
+
+		$vars['v'] = self::API_VERSION;
+		$vars['access_token'] = $this->access_token;
+
+		$params = http_build_query($vars);
+
+		$url = $this->http_build_query($method, $params);
+
+		return (array)$this->call($url);
+	}
+
+	/**
+	 * @param $method
+	 * @param string $params
+	 * @return string
+	 */	
+	private function http_build_query($method, $params = ''){
+		return  self::METHOD_URL . $method . '?' . $params;
+	}
+
+	/**
+	 * @param string $url
+	 * @return bool|mixed|string
+	 * @throws VkException
+	*/
+	private function call($url = ''){
+		if(function_exists('curl_init')) $json = $this->curl_post($url); else $json = file_get_contents($url);
+
+		$json = json_decode($json, true);
+
+		// Ошибки, которые могут возникнуть описаны тут: https://vk.com/dev/errors
+		if(isset($json['error'])){
+			if($json['error']['error_msg'] != "Captcha needed") {
+				if (isset($json['error'], $json['error']['error_msg'], $json['error']['error_code'])) {
+					throw new VkException($json['error']['error_msg'], $json['error']['error_code']);
+				}
+			}else{
+				echo "Error detected! Need captcha! Captcha img: ".$json['error']['captcha_img'];
+			}
+		}
+
+		if(isset($json['response'])) return $json['response'];
+
+		return $json;
     }
+		
+	private function curl_post($url){
 
-    public function init_classes(){
-        $this->audios = new VkAudios($this->access_token);
-        //TODO: More Classes
-    }
+		if(!function_exists('curl_init')) return false;
 
+		$param = parse_url($url);
 
-    /**
-     * @return null|string
-     */
-    public function getAccessToken(){
-        return $this->access_token;
-    }
+		if( $curl = curl_init() ) {
 
-    /**
-     * @param null|string $access_token
-     */
-    public function setAccessToken($access_token){
-        $this->access_token = $access_token;
-    }
+			curl_setopt($curl, CURLOPT_URL, $param['scheme'].'://'.$param['host'].$param['path']);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $param['query']);
+			$out = curl_exec($curl);
 
-    /**
-     * @return array
-     */
-    public function getScopes(){
-        return $this->scopes;
-    }
+			curl_close($curl);
 
-    /**
-     * @param array $scopes
-     */
-    public function setScopes($scopes){
-        $this->scopes = $scopes;
-    }
+			return $out;
+		}
 
-    /**
-     * @param string $method - �����, http://vk.com/dev/methods
-     * @param array $vars
-     * @return array
-     */
-    public function api($method = '', array $vars = array()){
+		return false;
+	}
 
-        $vars['v'] = self::API_VERSION;
-        $vars['access_token'] = $this->access_token;
-
-        $params = http_build_query($vars);
-
-        $url = $this->http_build_query($method, $params);
-
-        return (array)$this->call($url);
-    }
-
-    /**
-     * @param $method
-     * @param string $params
-     * @return string
-     */
-    private function http_build_query($method, $params = ''){
-        return  self::METHOD_URL . $method . '?' . $params;
-    }
-
-    /**
-     * @param string $url
-     * @return bool|mixed|string
-     * @throws VkException
-     */
-    private function call($url = ''){
-        if(function_exists('curl_init')) $json = $this->curl_post($url); else $json = file_get_contents($url);
-
-        $json = json_decode($json, true);
-
-        // Ошибки, которые могут возникнуть описаны тут: https://vk.com/dev/errors
-        if($json['error']['error_msg'] != "Captcha needed") {
-            if (isset($json['error'], $json['error']['error_msg'], $json['error']['error_code'])) {
-                throw new VkException($json['error']['error_msg'], $json['error']['error_code']);
-            }
-        }else{
-            echo "Error detected! Need captcha! Captcha img: ".$json['error']['captcha_img'];
-        }
-
-        if(isset($json['response'])) return $json['response'];
-
-        return $json;
-    }
-
-    private function curl_post($url){
-
-        if(!function_exists('curl_init')) return false;
-
-        $param = parse_url($url);
-
-        if( $curl = curl_init() ) {
-
-            curl_setopt($curl, CURLOPT_URL, $param['scheme'].'://'.$param['host'].$param['path']);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $param['query']);
-            $out = curl_exec($curl);
-
-            curl_close($curl);
-
-            return $out;
-        }
-
-        return false;
-    }
-
-    public function getAudios(){
-        return $this->audios;
-    }
+	public function getAudios(){
+		return $this->audios;
+	}
 
 }
